c2102acabe92c44949b613c584a2d93d99b8c873
raymondYsheng <bobisabob82@gmail.com>
2024-01-31 13:23:40 -0800

    ocr functions w/o handwriting

--- src/main/java/com/nighthawk/spring_portfolio/mvc/imagerec/ImageApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/imagerec/ImageApiController.java
@@ -3,27 +3,26 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Base64;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import java.io.File;
 
-import lombok.RequiredArgsConstructor;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 
 @RestController
 @RequestMapping("/image")
 public class ImageApiController {
+
+    @Autowired
+    private OCRService ocrService;
 
     @Value("${java.io.tmpdir}")
     private String tempUploadDir;
@@ -46,11 +45,10 @@ public class ImageApiController {
             // Move the file to the final destination
             String finalFilePath = uploadDir + File.separator + file.getOriginalFilename();
             new File(tempFilePath).renameTo(new File(finalFilePath));
-
             
+            return ResponseEntity.ok(ocrService.performOcr(finalFilePath));
+            // return ResponseEntity.ok("Hi");
 
-
-            return ResponseEntity.ok("File uploaded successfully!");
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to upload file");
         }
