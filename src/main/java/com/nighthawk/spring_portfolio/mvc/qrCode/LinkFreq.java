520def64f04eff475baeac727de8779e6d37d4ef
Toby-Leeder <tobyzeavleeder@gmail.com>
2024-02-01 12:14:06 -0800

    turn linkfreq into @embeddable

--- src/main/java/com/nighthawk/spring_portfolio/mvc/qrCode/LinkFreq.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/qrCode/LinkFreq.java
@@ -8,12 +8,11 @@ import java.util.ArrayList;
 
 import jakarta.persistence.*;
 
-@Data  // Annotations to simplify writing code (ie constructors, setters)
+@Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity // Annotation to simplify creating an entity, which is a lightweight persistence domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
 public class LinkFreq {
-
+    
     private String link;
     private double frequency;
 
