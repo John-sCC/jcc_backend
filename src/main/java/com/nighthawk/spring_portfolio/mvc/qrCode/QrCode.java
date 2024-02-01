a634e09b5ae8f32ab05294d5e570fceb5d3e6b16
Toby-Leeder <tobyzeavleeder@gmail.com>
2024-02-01 11:49:30 -0800

    created qrCode pojo

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/qrCode/QrCode.java
@@ -0,0 +1,26 @@
+package com.nighthawk.spring_portfolio.mvc.qrCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+import jakarta.persistence.*;
+
+@Data  // Annotations to simplify writing code (ie constructors, setters)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity // Annotation to simplify creating an entity, which is a lightweight persistence domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
+public class QrCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private ArrayList<LinkFreq> links;
+
+    public void addLink(LinkFreq linkFreq ){
+        links.add(linkFreq);
+        return;
+    }
+}
