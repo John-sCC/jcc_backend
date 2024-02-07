dde9e5dbb209b4e1c36c7abb86928283c1c6d725
Drew Reed <drewdafox@gmail.com>
2024-02-07 08:49:46 -0800

    deployment backend uses deployed frontend

--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
 					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
 					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
 					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET", "OPTIONS", "HEAD"))
-					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "https://john-scc.github.io", "http://localhost:4100"))
+					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "https://john-scc.github.io"))//"http://localhost:4100"))
 				)
 				.formLogin(form -> form 
 					.loginPage("/login")
