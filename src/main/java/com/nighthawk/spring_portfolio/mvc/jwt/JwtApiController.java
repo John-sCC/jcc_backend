22e9c32417f15f2b2307e99de1e97d6ee9c55df6
Drew Reed <drewdafox@gmail.com>
2024-02-28 10:07:38 -0800

    test for cookie verification

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
@@ -2,6 +2,7 @@ package com.nighthawk.spring_portfolio.mvc.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,10 +10,15 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 
 import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
@@ -39,13 +45,25 @@ public class JwtApiController {
 		final ResponseCookie tokenCookie = ResponseCookie.from("jwt", token)
 			.httpOnly(true)
 			.secure(true)
-			.path("/jcc_frontend")
+			.path("/")
 			.maxAge(3600)
 			.sameSite("None; Secure")
 			// .domain("example.com") // Set to backend domain
 			.build();
 		return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, tokenCookie.toString()).body(new TokenResponse(token));
 	}
+
+	@GetMapping("/cookie")
+    public ResponseEntity<Person> getPersonWithCookie(@CookieValue(name = "jwt") String jwtToken) {
+		// get user's username
+        String decodedUsername = jwtTokenUtil.getUsernameFromToken(jwtToken);
+		if (personDetailsService.getByEmail(decodedUsername) != null) {
+			Person person = personDetailsService.getByEmail(decodedUsername);
+			return new ResponseEntity<>(person, HttpStatus.OK);
+		}
+		// not found
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 
 	private void authenticate(String username, String password) throws Exception {
 		try {
