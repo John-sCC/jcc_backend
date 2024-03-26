a1f447b97ddf82197b76111004441c02e637d2ef
raymondYsheng <bobisabob82@gmail.com>
2024-03-26 11:54:09 -0700

    place it on hold for now

--- src/main/java/com/nighthawk/spring_portfolio/mvc/statsData/Quantitative.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/statsData/Quantitative.java
@@ -1,13 +1,21 @@
 package com.nighthawk.spring_portfolio.mvc.statsData;
 
-/*
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.springframework.data.mongodb.core.schema.JsonSchemaObject.Type.JsonType;
+
+import com.nighthawk.spring_portfolio.mvc.classPeriod.ClassPeriod;
 
 import groovyjarjarantlr4.runtime.misc.Stats;
 import jakarta.persistence.*;
@@ -16,58 +24,46 @@ import jakarta.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Convert(attributeName ="person", converter = JsonType.class)
 public class Quantitative extends StatsFunctions {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private List<String> variable;
-
-    @Lob
-    private byte[] values;
+    @NonNull
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private Map<String, List<Double>> input = new HashMap<>(); //variable name, manually input data in a list
 
     private double correlation;
 
-    public Quantitative(List<String> variableNames, List<List<Double>> variableValues) {
-        if (variableNames.size() != variableValues.size()) {
-            throw new IllegalArgumentException("Lists must have the same size");
-        }
-        this.variable = variableNames;
-        //this.values = serializeValues(variableValues);
-        correlation = calculateCorrelation(variableValues.get(0), variableValues.get(1));
+    public Quantitative(Map<String, List<Double>> input) {
+        this.input = input;
+        
+        correlation = calculateCorrelation(input.get(0), input.get(1));
     }
 
     public double getCorrelation() {
         return this.correlation;
     }
 
-    private byte[] serializeValues(List<List<Double>> values) {
-        // Serialize the list of lists into byte array (e.g., using JSON serialization)
-        // You can use libraries like Jackson ObjectMapper for JSON serialization
-        // Convert values to JSON string and then to byte array
-        // For simplicity, assuming JSON serialization here
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString(values);
-            return json.getBytes(StandardCharsets.UTF_8);
-        } catch (JsonProcessingException e) {
-            throw new IllegalArgumentException("Error serializing values to JSON", e);
-        }
+    public static Quantitative[] init() {
+        Map<String, List<Double>> inputData = new HashMap<>();
+        List<Double> dataList1 = Arrays.asList(1.0, 2.0, 3.0);
+        List<Double> dataList2 = Arrays.asList(4.0, 5.0, 6.0);
+        inputData.put("data1", dataList1);
+        inputData.put("data2", dataList2);
+
+        Quantitative quan = new Quantitative();
+        quan.setInput(inputData);
+
+        Quantitative quantitative[] = {quan};
+
+        return(quantitative);
     }
 
-    private List<List<Double>> deserializeValues(byte[] serializedValues) {
-        // Deserialize the byte array back to list of lists (e.g., using JSON deserialization)
-        // You can use libraries like Jackson ObjectMapper for JSON deserialization
-        // Convert byte array to JSON string and then deserialize
-        // For simplicity, assuming JSON deserialization here
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = new String(serializedValues, StandardCharsets.UTF_8);
-            return objectMapper.readValue(json, new TypeReference<List<List<Double>>>() {});
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Error deserializing values from JSON", e);
-        }
+    public static void main(String[] args) {
+        Quantitative quan[] = init();
     }
 }
-*/
--- src/main/java/com/nighthawk/spring_portfolio/mvc/statsData/StatsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/statsData/StatsApiController.java
@@ -1,29 +1,46 @@
-package com.nighthawk.spring_portfolio.mvc.statsData;
+// package com.nighthawk.spring_portfolio.mvc.statsData;
 
-import org.hibernate.Hibernate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import org.hibernate.Hibernate;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-import jakarta.transaction.Transactional;
+// import jakarta.transaction.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Map;
+// import java.util.Optional;
 
-@RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/stats")
-public class StatsApiController {
-    /*
-    @GetMapping("/get")
-    public ResponseEntity<List<double>> getCorrelation() {
-        List<Quantitative> quan = repository.findAll();
-        List<double> correlations = new ArrayList<>();
-        for (int i=0;i<quan.size();i++){
-            correlations.add(quan.get(i).getCorrelation());
-        } 
-        return correlations;
-    }
-    */
-}
+// @RestController // annotation to simplify the creation of RESTful web services
+// @RequestMapping("/api/stats")
+// public class StatsApiController {
+//     /*
+//     @GetMapping("/get")
+//     public ResponseEntity<List<double>> getCorrelation() {
+//         List<Quantitative> quan = repository.findAll();
+//         List<double> correlations = new ArrayList<>();
+//         for (int i=0;i<quan.size();i++){
+//             correlations.add(quan.get(i).getCorrelation());
+//         } 
+//         return correlations;
+//     }
+//     */
+//     private final Quantitative quantitativeService;
+
+//     @Autowired
+//     public StatsApiController(Quantitative quantitativeService) {
+//         this.quantitativeService = quantitativeService;
+//     }
+
+//     @PostMapping("/input")
+//     public ResponseEntity<Quantitative> addQuantitativeData(@RequestBody Quantitative inputData) {
+//         try {
+//             Quantitative quantitative = quantitativeService.setInput(inputData);
+//             return new ResponseEntity<>(quantitative, HttpStatus.CREATED);
+//         } catch (Exception e) {
+//             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//         }
+//     }
+// }
