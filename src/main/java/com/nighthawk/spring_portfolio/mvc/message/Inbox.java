5dca60f331577b420c7458dbf565163f810b695d
Your Name <ryanrob327@gmail.com>
2024-04-17 19:02:21 -0700

    message get map and message sort by id

--- src/main/java/com/nighthawk/spring_portfolio/mvc/message/Inbox.java
+++
@@ -1,5 +0,0 @@
-package mvc.message;
-
-public class Inbox {
-    
-}
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/message/Message.java
@@ -0,0 +1,22 @@
+package com.nighthawk.spring_portfolio.mvc.message;
+
+
+public class Message {
+    private Long id;
+    private String from;
+    private String subject;
+    private String content;
+
+    // Constructor with parameters
+    public Message(Long id, String from, String subject, String content) {
+        this.id = id;
+        this.from = from;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+}
