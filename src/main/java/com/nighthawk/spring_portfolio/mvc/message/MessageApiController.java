d7de1cd1beafcfcaa40d549437d061b727084eb7
Your Name <ryanrob327@gmail.com>
2024-05-03 09:14:35 -0700

    added basic POST

--- src/main/java/com/nighthawk/spring_portfolio/mvc/message/MessageApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/message/MessageApiController.java
@@ -2,6 +2,8 @@ package com.nighthawk.spring_portfolio.mvc.message;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,6 +20,7 @@ public class MessageApiController {
         messages.add(new Message(3L, "sender3@example.com", "Aiden Huynh", "slash slash slash!"));
         messages.add(new Message(1L, "sender1@example.com", "RiderCella", "Slopping their Dripper"));
         messages.add(new Message(2L, "sender2@example.com", "Subject 2", "Message 2 content"));
+        messages.add(new Message(5L, "test2", "test2", "test2"));
 
         // Sort messages by ID
         Collections.sort(messages, (m1, m2) -> m1.getId().compareTo(m2.getId()));
@@ -37,5 +40,13 @@ public class MessageApiController {
             }
         }
         return null; // Message not found
+    }
+
+    @PostMapping("/api/messages")
+    public void createMessage(@RequestBody Message message) {
+        // Logic to save the message (e.g., save it to a database)
+        // You can implement this logic based on your requirements
+        System.out.println("Received new message: " + message);
+        messages.add(new Message(4L, message.getFrom(), message.getSubject(), message.getContent()));
     }
 }
