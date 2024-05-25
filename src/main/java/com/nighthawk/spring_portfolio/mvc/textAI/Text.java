9998cc7b2b2278780deb54ab70841423cbdf256a
Haoxuan Tong <haoxuantong8@gmail.com>
2024-05-24 22:31:38 -0700

    modified:   src/main/java/com/nighthawk/spring_portfolio/mvc/textAI/Text.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/textAI/Text.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/textAI/Text.java
@@ -1,68 +1,44 @@
 package com.nighthawk.spring_portfolio.mvc.textAI;
 
 import java.time.LocalTime;
-import java.time.ZoneId;
+import java.util.ArrayList;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  // Annotations to simplify writing code (ie constructors, setters)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity // Annotation to simplify creating an entity, which is a lightweight persistence domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
 public class Text {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique=true)
     private String name;
+    @Column
     private String text;
+    @Column
     private LocalTime timeUpdated;
+    @Column
     private boolean tested;
+    @Column
     private int score;
 
-    // Constructors (including a no-arg constructor)
-
-    public Text() {
-    }
-
-    public Text(String name, String text) {
-        this.name = name;
-        this.text = text;
-        ZoneId temp = ZoneId.of("America/Los_Angeles");
-        this.timeUpdated = LocalTime.now(temp);
-        this.tested = false;
-        this.score = 0;
-    }
-
-    // Getters and Setters
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getText() {
-        return this.text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public LocalTime getTimeUpdated() {
-        return this.timeUpdated;
-    }
-
-    public void setTimeUpdated(LocalTime timeUpdated) {
-        this.timeUpdated = timeUpdated;
-    }
-
-    public boolean getTested() {
-        return this.tested;
-    }
-
-    public void setTested(boolean tested) {
-        this.tested = tested;
-    }
-
-    public int getScore() { 
-        return this.score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
+    public static Text[] init() {
+        Text t1 = new Text();
+        t1.setName("New Text");
+        t1.setText("");
+        t1.setTimeUpdated(LocalTime.MIDNIGHT);
+        t1.setTested(false);
+        t1.setScore(0);
+        Text texts[] = {t1};
+        return texts;
     }
 }
