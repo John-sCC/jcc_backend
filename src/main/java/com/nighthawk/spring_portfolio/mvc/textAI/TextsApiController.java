296b9dda3b3e40de17f5cde4c548e486246ec348
Haoxuan Tong <haoxuantong8@gmail.com>
2024-05-29 22:52:46 -0700

    debug 3

--- src/main/java/com/nighthawk/spring_portfolio/mvc/textAI/TextsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/textAI/TextsApiController.java
@@ -1,4 +1,4 @@
-package com.nighthawk.spring_portfolio.mvc.textAI;
+/*package com.nighthawk.spring_portfolio.mvc.textAI;
 
 import java.time.LocalTime;
 import java.time.ZoneId;
@@ -99,3 +99,4 @@ public class TextsApiController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
+*/
